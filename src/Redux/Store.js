83838e5588da2700d619ab5082fbfb38967d4b79
Kani <kanimozhibe02@gmail.com>
2023-12-14 22:16:10 +0530

    Updates

--- src/Redux/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import stateSlice from "./stateSlice";
-export const store = configureStore({
-    reducer : {
-        data : stateSlice,
-    },
-})
